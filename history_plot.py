5dd3483007058c489fffef5c023d9f8f66cb2dc5
Jean-Eric Campagne <campagne@lal.in2p3.fr>
2020-01-14 10:34:45 +0100

    add log scale option for losses

--- history_plot.py
+++ history_plot.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='loss-plot')
 parser.add_argument('--file', type=str, required=True)
 parser.add_argument('--tag', type=str, required=True)
+parser.add_argument('--log', default=False,action='store_true')
 
 args = parser.parse_args()
 data = np.load(args.file)
@@ -15,6 +16,8 @@ test_psnr = data[2]
 fig, ax1 = plt.subplots()
 ax1.plot(train_loss, label="train", color='tab:blue')
 ax1.plot(test_loss, label="test", color='tab:orange')
+if args.log:
+    ax1.set_yscale("log")
 ax1.set_xlabel('epoch')
 ax1.set_ylabel('Cross Entropy loss')
 ax1.tick_params(axis='y')
